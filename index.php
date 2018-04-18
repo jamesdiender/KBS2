2dede609597fe3c425a11ac78f7509c234cdb416
Tom S <tomschelhaas@hotmail.com>
2018-04-18 12:19:11 +0200

    Index.php added

--- index.php
+++ index.php
@@ -111,7 +111,7 @@ if (isset($_POST['formSubmit'])) {
     $message = $messageGroet . $messageTekst . $messageOpleiding;
     echo $message;
 
-    require '../assets/vendor/autoload.php';
+    require 'assets/vendor/autoload.php';
 
     $mail = new PHPMailer(true);                                // Passing `true` enables exceptions
     try {
@@ -123,7 +123,7 @@ if (isset($_POST['formSubmit'])) {
                 'allow_self_signed' => true
             )
         );
-        $mail->SMTPDebug = 0;                                   // Enable verbose debug output
+        $mail->SMTPDebug = 2;                                   // Enable verbose debug output
         $mail->isSMTP();                                        // Set mailer to use SMTP
         $mail->Host = '127.0.0.1';                              // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                                 // Enable SMTP authentication
