6e1cb2f70f7a5dec4aebf3197c404b96a9f69242
Koen <koendiender@gmail.com>
2018-05-16 19:54:19 +0200

    f

--- index.php
+++ index.php
@@ -123,7 +123,7 @@ if (isset($_POST['formSubmit'])) {
                 'allow_self_signed' => true
             )
         );
-        $mail->SMTPDebug = 0;                                   // Enable verbose debug output
+        $mail->SMTPDebug = 3;                                   // Enable verbose debug output
         $mail->isSMTP();                                        // Set mailer to use SMTP
         $mail->Host = '192.168.1.5';                              // Specify main and backup SMTP servers
         $mail->SMTPAuth = false;                                // Enable SMTP authentication
